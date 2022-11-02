46595c3fd151a54156468d57c12642ff5b1f0b18
JeongBG <117093635+JeongBG@users.noreply.github.com>
2022-11-03 08:04:55 +0900

    Create cnn_crawling.py

---
+++ cnn_crawling.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+Year=22
+Month=11
+Day=2
+
+Year=str(Year)
+Month=str(Month)
+Day=str(Day)
+
+Month=Month.zfill(2)
+Day=Day.zfill(2)
+
+url='http://transcripts.cnn.com/TRANSCRIPTS/{}/{}/sn.01.html'.format(Year+Month,Day)
+
+resp=requests.get(url)
+soup=BeautifulSoup(resp.text)
+print('Request code is {}'.format(resp.status_code))
+
+container=soup.find('div', style='padding-left:10px;').find_all('p',class_='cnnBodyText')
+
+contents=''
+for i in container:
+    contents+='\n'+ i.get_text().strip()
+
+contents=contents.replace("`","'")
+contents=contents.replace('   ',' ')
+
+file=open('{}{}{}_CNN.txt'.format(Year,Month,Day),'w')
+file.write(contents)
+file.close()
