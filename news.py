9f4aeed1df32b6c4c4cf403ac3e7013f2369467c
JeongBG <117093635+JeongBG@users.noreply.github.com>
2022-11-03 11:18:08 +0900

    Update news.py

--- news.py
+++ news.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 titles = []
-for j in range(1, 100, 10):
+for j in range(1, 50, 10):
     res = requests.get('https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%86%8D%EB%B3%B4&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=32&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=' + str(j))
     soup = BeautifulSoup(res.text, 'lxml')
     title = soup.select('a.news_tit')
